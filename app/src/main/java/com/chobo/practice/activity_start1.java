9c6ba87fa84caa58fa79502531c1a9f966d053a6
jungwooseong <32189141+jungwooseong@users.noreply.github.com>
2021-12-03 03:36:59 +0900

    030336 오류버전

--- app/src/main/java/com/chobo/practice/activity_start1.java
+++ app/src/main/java/com/chobo/practice/activity_start1.java
@@ -1,20 +1,45 @@
 package com.chobo.practice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+
 public class activity_start1 extends AppCompatActivity {
+
+    final String TAG = "MainActivity";
+    public String dataKey = "t6eds%2FQ%2B1LeB%2BpXNB9%2BrKW0b2NMjbEj1SfRd3kIGJSHK1Jb%2BBLJ8Hrod3dKOrnPgeX1%2FIau2RbhMKOq7J8pVKg%3D%3D";
+    private String requestUrl;
+    ArrayList<Item> list = null;
+    Item bus = null;
+    RecyclerView recyclerView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start1);
 
-        Button btn_notification = (Button)findViewById(R.id.btn_notification);
+        Button btn_notification = (Button) findViewById(R.id.btn_notification);
+        Button btn_select = (Button) findViewById(R.id.btn_select);
+        Button btn_all = (Button) findViewById(R.id.btn_all);
+        Button btn_claim = (Button) findViewById(R.id.btn_claim);
+
         btn_notification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,7 +48,6 @@ public class activity_start1 extends AppCompatActivity {
             }
         });
 
-        Button btn_select = (Button)findViewById(R.id.btn_select);
         btn_select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,7 +56,6 @@ public class activity_start1 extends AppCompatActivity {
             }
         });
 
-        Button btn_all = (Button)findViewById(R.id.btn_all);
         btn_all.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,7 +64,6 @@ public class activity_start1 extends AppCompatActivity {
             }
         });
 
-        Button btn_claim = (Button)findViewById(R.id.btn_claim);
         btn_claim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,5 +71,103 @@ public class activity_start1 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+//        AsyncTask
+        MyAsyncTask myAsyncTask = new MyAsyncTask();
+        myAsyncTask.execute();
+
+
     }
+
+
+    public class MyAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            requestUrl = "http://apis.data.go.kr/6410000/busarrivalservice/getBusArrivalList?serviceKey=" +dataKey+ "&stationId=228000710";
+            try {
+                boolean b_locationNo1 = false; //첫번째 차량 위치정보
+                boolean b_plateNo1 =false;
+                boolean b_routeId = false;
+                boolean b_stationId = false;
+                boolean b_predictTime1 = false; //첫번째 차량 도착예상시간
+
+                URL url = new URL(requestUrl);
+                InputStream is = url.openStream();
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                XmlPullParser parser = factory.newPullParser();
+                parser.setInput(new InputStreamReader(is, "UTF-8"));
+
+                String tag;
+                int eventType = parser.getEventType();
+
+                while(eventType != XmlPullParser.END_DOCUMENT){
+                    switch (eventType){
+                        case XmlPullParser.START_DOCUMENT:
+                            list = new ArrayList<Item>();
+                            break;
+                        case XmlPullParser.END_DOCUMENT:
+                            break;
+                        case XmlPullParser.END_TAG:
+                            if(parser.getName().equals("busArrivalList") && bus != null) {
+                                list.add(bus);
+                            }
+                            break;
+                        case XmlPullParser.START_TAG:
+                            if(parser.getName().equals("busArrivalList")){
+                                bus = new Item();
+                            }
+                            if (parser.getName().equals("locationNo1")) b_locationNo1 = true;
+                            if (parser.getName().equals("plateNo1")) b_plateNo1 = true;
+                            if (parser.getName().equals("routeId")) b_routeId = true;
+                            if (parser.getName().equals("stationId")) b_stationId = true;
+                            if (parser.getName().equals("predictTime1")) b_predictTime1 = true;
+                            break;
+                        case XmlPullParser.TEXT:
+                            if(b_locationNo1){
+                                bus.setLocationNo1(parser.getText());
+                                b_locationNo1 = false;
+                            } else if(b_plateNo1) {
+                                bus.setPlateNo1(parser.getText());
+                                b_plateNo1 = false;
+                            } else if (b_routeId) {
+                                bus.setRouteId(parser.getText());
+                                b_routeId = false;
+                            } else if(b_stationId) {
+                                bus.setStationId(parser.getText());
+                                b_stationId = false;
+                            } else if(b_predictTime1) {
+                                bus.setPredictTime1(parser.getText());
+                                b_predictTime1 = false;
+                            }
+                            break;
+                    }
+                    eventType = parser.next();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            //어답터 연결
+            MyAdapter adapter = new MyAdapter(getApplicationContext(), list);
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+
 }
